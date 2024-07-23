a348c4586cc3259ec7147ff3582dbff4ac5f3a31
DmitriyRadozhitskiy <d.rado1991@gmail.com>
2024-07-23 12:15:55 +0300

    init

---
+++ main.tf
@@ -0,0 +1,142 @@
+terraform {
+  required_providers {
+    selectel = {
+      source  = "selectel/selectel"
+      version = "5.1.1"
+    }
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "2.0.0"
+    }
+  }
+}
+
+variable "selectel_password" {}
+variable "selectel_radozhickij_uuid" {}
+variable "service_user_uid" {}
+
+provider "selectel" {
+  domain_name = "61244"
+  username    = "Radozhitskiy"
+  password    = var.selectel_password
+}
+
+provider "openstack" {
+  auth_url    = "https://cloud.api.selcloud.ru/identity/v3"
+  domain_name = "61244"
+  tenant_id   = var.selectel_radozhickij_uuid
+  user_name   = "Radozhitskiy"
+  password    = var.selectel_password
+  region      = "ru-9"
+}
+
+resource "selectel_vpc_keypair_v2" "keypair_1" {
+  name       = "keypair"
+  public_key = file("~/.ssh/the-key.pub")
+  user_id    = var.service_user_uid
+}
+
+#resource "openstack_compute_flavor_v2" "flavor_1" {
+#  name      = "custom-flavor-with-network-volume"
+#  vcpus     = 2
+#  ram       = 4096
+#  disk      = 0
+#  flavor_id = "SL1.2-4096"
+#  is_public = false
+#
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
+
+variable "radozhickij_network_id" {
+  default = ""
+}
+
+variable "radozhickij_subnet_id" {
+  default = ""
+}
+
+resource "openstack_networking_port_v2" "port_1" {
+  name       = "port"
+  network_id = var.radozhickij_network_id
+
+  fixed_ip {
+    subnet_id = var.radozhickij_subnet_id
+  }
+}
+
+data "openstack_images_image_v2" "image_1" {
+  name        = "Ubuntu 20.04 LTS 64-bit"
+  most_recent = true
+  visibility  = "public"
+}
+
+resource "openstack_blockstorage_volume_v3" "volume_1" {
+  name                 = "boot-volume-for-server"
+  size                 = "5"
+  image_id             = data.openstack_images_image_v2.image_1.id
+  volume_type          = "fast.ru-9a"
+  availability_zone    = "ru-9a"
+  enable_online_resize = true
+
+  lifecycle {
+    ignore_changes = [image_id]
+  }
+
+}
+
+resource "openstack_blockstorage_volume_v3" "volume_2" {
+  name                 = "additional-volume-for-server"
+  size                 = "7"
+  volume_type          = "universal.ru-9a"
+  availability_zone    = "ru-9a"
+  enable_online_resize = true
+}
+
+resource "openstack_compute_instance_v2" "server_1" {
+  name              = "server"
+  flavor_id         = "1013" #"SL1.2-4096" #openstack_compute_flavor_v2.flavor_1.id
+  key_pair          = selectel_vpc_keypair_v2.keypair_1.name
+  availability_zone = "ru-9a"
+
+  network {
+    port = openstack_networking_port_v2.port_1.id
+  }
+
+  lifecycle {
+    ignore_changes = [image_id]
+  }
+
+  block_device {
+    uuid             = openstack_blockstorage_volume_v3.volume_1.id
+    source_type      = "volume"
+    destination_type = "volume"
+    boot_index       = 0
+  }
+
+  block_device {
+    uuid             = openstack_blockstorage_volume_v3.volume_2.id
+    source_type      = "volume"
+    destination_type = "volume"
+    boot_index       = -1
+  }
+
+  vendor_options {
+    ignore_resize_confirmation = true
+  }
+}
+
+resource "openstack_networking_floatingip_v2" "floatingip_1" {
+  pool = "external-network"
+}
+
+resource "openstack_networking_floatingip_associate_v2" "association_1" {
+  port_id     = openstack_networking_port_v2.port_1.id
+  floating_ip = openstack_networking_floatingip_v2.floatingip_1.address
+}
+
+output "public_ip_address" {
+  value = openstack_networking_floatingip_v2.floatingip_1.fixed_ip  #address
+}
+
